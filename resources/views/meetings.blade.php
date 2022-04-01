b9f82ce9d005ef6985aaa345bd12b405ad4de495
ChristianZamora19 <christian_1320114025@uptecamac.edu.mx>
2022-03-31 18:57:26 -0600

    LooL

--- resources/views/meetings.blade.php
+++ resources/views/meetings.blade.php
@@ -137,7 +137,7 @@ https://templatemo.com/tm-569-edu-meeting
                   <a href=""><img src="/images/single-meeting.jpg" alt=""></a>
                 </div>
                 <div class="down-content">
-                  <a href=""><h4><?php echo $mostrar['Nombre']?></h4></a>
+                  <<h4> <?php echo $mostrar['Nombre']?></h4>
                   <p>Director:<?php echo $mostrar['Director']?></p>
                   <p>Descripcion:<?php echo $mostrar['Descripcion']?></p>
                   <div class="row">
